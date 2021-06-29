125af6ae0bae08d46513447ce8d408bef4e60c5e
Oleg Baranov <oleg.baranov@skywindgroup.com>
2021-06-29 18:06:27 +0300

    feat: lessons 59-61: shoow spinner and error indicator

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,12 @@
+import './spinner.css';
+import React from 'react';
+
+export default function Spinner() {
+    return (
+        <div className="loadingio-spinner-eclipse-h4g3m4jx8rw">
+            <div className="ldio-mtvyi4wyux">
+                <div></div>
+            </div>
+        </div>
+    )
+}
