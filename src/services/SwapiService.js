c2bde3bfb740e892fe5341917e0efb73639a22a3
Oleg Baranov <oleg.baranov@skywindgroup.com>
2021-06-29 16:12:39 +0300

    feat: lessons 57-58: show random planet

--- src/services/SwapiService.js
+++ src/services/SwapiService.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export default class SwapiService {
 
     _apiBase = 'https://swapi.dev/api';
@@ -11,32 +13,73 @@ export default class SwapiService {
       }
     }
   
-    async getAllPeopple() {
-      const resp = await this.getResource(`/people`);
-      return resp.results;
+    async getAllPeople() {
+      const res = await this.getResource(`/people/`);
+      return res.results.map(this._transformPerson);
     }
   
     async getPerson(id) {
-      return await this.getResource(`/people/${id}/`);
+      const person = await this.getResource(`/people/${id}/`);
+      return this._transformPerson(person);
     }
   
     async getAllPlanets() {
       const resp = await this.getResource(`/planets`);
-      return resp.results;
+      return resp.results.map(this._transformPlanet);
     }
   
     async getPlanet(id) {
-      return await this.getResource(`/planets/${id}/`);
+      const planet = await this.getResource(`/planets/${id}/`);
+      return this._transformPlanet(planet);
     }
   
     async getAllStarships() {
-      const resp = await this.getResource(`/starships`);
-      return resp.results;
+      const res = await this.getResource(`/starships/`);
+      return res.results.map(this._transformStarship);
     }
   
     async getStarship(id) {
-      return await this.getResource(`/starships/${id}/`);
+      const starship = this.getResource(`/starships/${id}/`);
+      return this._transformStarship(starship);
     }
   
+
+    _transformPlanet(planet) {
+      return {
+        id: this._extractId(planet),
+        name: planet.name,
+        population: planet.population,
+        rotationPeriod: planet.rotation_period,
+        diameter: planet.diameter
+      }
+    }
+
+    _transformStarship(starship) {
+      return {
+        id: this._extractId(starship),
+        name: starship.name,
+        model: starship.model,
+        manufacturer: starship.manufacturer,
+        costInCredits: starship.costInCredits,
+        length: starship.length,
+        crew: starship.crew,
+        passengers: starship.passengers,
+        cargoCapacity: starship.cargoCapacity
+      }
+    }
+  
+    _transformPerson(person) {
+      return {
+        id: this._extractId(person),
+        name: person.name,
+        gender: person.gender,
+        birthYear: person.birthYear,
+        eyeColor: person.eyeColor
+      }
+    }
+
+    _extractId(item) {
+      return item.url.match(/\/([0-9])*\/$/)[1];
+    }
   }
   
