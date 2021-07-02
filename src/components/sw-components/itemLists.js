5bae568e4b1d60ee0ff4630d4843be0b3008df60
Oleg Baranov <oleg.baranov@skywindgroup.com>
2021-07-02 18:06:12 +0300

    refactoring: lesson 90: create and use compose for hoc

--- src/components/sw-components/itemLists.js
+++ src/components/sw-components/itemLists.js
@@ -1,18 +1,9 @@
 
+import compose from '../hocHelpers/compose';
+import withChildFunction from '../hocHelpers/withChildFunction';
 import withData from '../hocHelpers/withData';
 import withSwapi from '../hocHelpers/withSwapi';
 import ItemList from '../itemList/itemList';
-
-
-const withChildFunction = (fn) => (Wrapped) => {
-    return (props) => {
-        return (
-            <Wrapped {...props}>
-                {fn}
-            </Wrapped>
-        )
-    }
-}
 
 const renderName = ({ name }) => <span>{name}</span>;
 const renderNameAndAge = ({ name, birthYear }) => <span>{name} ({birthYear})</span>;
@@ -28,18 +19,20 @@ const mapStarshipMethodsToProps = swapi => ({
     getData: swapi.getAllStarships
 });
 
-export const PersonList = withSwapi(mapPersonMethodsToProps)(
-    withData(
-        withChildFunction(renderNameAndAge)(ItemList)
-    )
-);
-export const PlanetList = withSwapi(mapPlanetMethodsToProps)(
-    withData(
-        withChildFunction(renderName)(ItemList)
-    )
-);
-export const StarshipList = withSwapi(mapStarshipMethodsToProps)(
-    withData(
-        withChildFunction(renderNameAndModel)(ItemList)
-    )
-);
+export const PersonList = compose(
+    withSwapi(mapPersonMethodsToProps),
+    withData,
+    withChildFunction(renderNameAndAge)
+    )(ItemList);
+
+export const PlanetList = compose(
+    withSwapi(mapPlanetMethodsToProps),
+    withData,
+    withChildFunction(renderName)
+    )(ItemList);
+export const StarshipList = compose(
+    withSwapi(mapStarshipMethodsToProps),
+    withData,
+    withChildFunction(renderNameAndModel)
+    )(ItemList);
+
